3b473101848b6d1d161b36fbb0c6bc670cb82348
dipesh maharjan <max.maharjan.2014@gmail.com>
2022-08-30 18:38:58 +0545

    initial commit

---
+++ routes/product.js
@@ -0,0 +1,80 @@
+const { verifyTokenAndAdmin } = require('./verifyToken');
+const Product = require('../models/Product')
+const router = require('express').Router();
+
+// CREATE
+router.post("/", verifyTokenAndAdmin, async (req, res) => {
+    const newProduct = new Product(req.body)
+
+    try {
+        const savedProduct = await newProduct.save();
+        res.status(200).json(savedProduct);
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+
+// //UPDATE
+// router.put("/:id", verifyTokenAndAuthorization, async (req, res) => {
+//     if (req.body.password) {
+//         req.body.password = CryptoJS.AES.encrypt(
+//             req.body.password,
+//             process.env.PASS_SEC
+//         ).toString();
+//     }
+
+//     try {
+//         const updatedUser = await User.findByIdAndUpdate(
+//             req.params.id,
+//             {
+//                 $set: req.body,
+//             },
+//             { new: true }
+//         );
+//         res.status(200).json(updatedUser);
+//     } catch (err) {
+//         res.status(500).json(err);
+
+//     }
+// });
+
+// // DELETE
+// router.delete("/:id", verifyTokenAndAuthorization, async (req, res) => {
+//     try {
+//         await User.findByIdAndDelete(req.params.id)
+//         res.status(200).json("user has been deleted !")
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
+
+// // GET USER
+// router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+//     try {
+//         const user = await User.findById(req.params.id)
+
+//         const { password, ...others } = user._doc;
+//         res.status(200).json(others)
+
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
+// // GET ALL USER
+// router.get("/", verifyTokenAndAdmin, async (req, res) => {
+//     const query = req.query.new;
+//     try {
+//         const user = query ? await User.find().sort({ _id: -1 }).limit(5) : await User.find();
+//         res.status(200).json(user)
+
+//     } catch (error) {
+//         res.status(500).json(error)
+//     }
+// })
+
+
+
+
+
+module.exports = router;
